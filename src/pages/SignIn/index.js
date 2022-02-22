34fe4a77a78d79ed980d968fd4d2bff1de921449
Julia Pinheiro de Farias <89882407+juliapinheiro42@users.noreply.github.com>
2022-02-22 17:04:57 -0300

    Update index.js

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -43,9 +43,9 @@ export default function SignIn() {
           <input type="password" placeholder="*******" value={Password} required onChange={(e) => setPassword(e.target.value) } />
           <button type="submit">Submit</button>
         </form>  
-        <Link className='d-none d-lg-block' to="/signin">Já tem uma conta? LogIn</Link>
+        <Link className='d-none d-lg-block' to="/signin">Não tem uma conta? Cadastre</Link>
       </div>
-      <Link className='d-lg-none mt-5' to="/signin">Já tem uma conta? LogIn</Link>
+      <Link className='d-lg-none mt-5' to="/signuo">Não tem uma conta? Cadastre</Link>
     </div>
   );
 }
